4ce5b8f9237e4a45447d38a48011fe552473c8fe
梁育修 <43335328+willy5423@users.noreply.github.com>
2018-12-24 14:30:42 +0800

    Create p6.cpp

---
+++ test 3/p6.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    char s1[1024], s2[1024];
+    while (scanf("%s %s", s1, s2) == 2)
+        printf("%d\n",
+               (((s2[0] - 'A') - (s1[0] - 'A'))%26 + 26)%26);
+    return 0;
+}
